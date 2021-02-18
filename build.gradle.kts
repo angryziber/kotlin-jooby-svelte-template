f1b8f1fe83141cbec3c159f97b5fb91a7ea07500
Anton Keks, Rain Sirp <anton@codeborne.com, rain@codeborne.com>
2021-02-18 15:32:41 +0200

    upgrade jvm2dts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,63 +8,15 @@ plugins {
   application
 }
 
-sourceSets {
-  getByName("main").apply {
-    java.srcDirs("src")
-    resources.srcDirs("src", "i18n", "ui/static").exclude("**/*.kt")
-  }
-  getByName("test").apply {
-    java.srcDirs("test")
-    resources.srcDirs("test").exclude("**/*.kt")
-  }
-}
-
-tasks.withType<KotlinCompile> {
-  kotlinOptions {
-    jvmTarget = "11"
-    freeCompilerArgs = listOf("-Xjsr305=strict -progressive")
-    javaParameters = true
-  }
-  finalizedBy("generateTSTypes")
-}
-
-val excludeClassNamesRegex = ".*(Service|Repository|Controller|Logger|Job|Module)\$"
-val packagesToGenerateTypesFor = "auth"
-
-tasks.register("generateTSTypes") {
-  dependsOn("classes")
-  doLast {
-    File("ui/api/types.ts").writeText(ByteArrayOutputStream().use { out ->
-      project.exec {
-        standardOutput = out
-        commandLine = """java -classpath ${sourceSets.main.get().runtimeClasspath.asPath}${File.pathSeparator}${sourceSets.main.get().compileClasspath.asPath}
-          jvm2dts.Main -exclude $excludeClassNamesRegex 
-          $packagesToGenerateTypesFor""".split("\\s+".toRegex())
-      }
-      out.toString()
-    })
-  }
-}
-
-val test by tasks.getting(Test::class) {
-  useJUnitPlatform()
-  exclude("ui/**")
-}
-
-tasks.register<Test>("e2eTest") {
-  useJUnitPlatform()
-  include("ui/**")
-  if (project.hasProperty("headless"))
-    systemProperties["chromeoptions.args"] = "--headless,--no-sandbox,--disable-gpu"
-  systemProperties["webdriver.chrome.logfile"] = "build/reports/chromedriver.log"
-  systemProperties["webdriver.chrome.verboseLogging"] = "true"
-}
-
 repositories {
   jcenter()
 }
 
 val joobyVersion = "2.9.5"
+
+val jvm2dts by configurations.creating {
+  extendsFrom(configurations.implementation.get())
+}
 
 dependencies {
   kapt("io.jooby:jooby-apt:$joobyVersion")
@@ -92,7 +44,59 @@ dependencies {
   testImplementation("com.codeborne:selenide:5.10.0")
   runtimeOnly("com.h2database:h2:1.4.200")
 
-  compileOnly("com.codeborne:jvm2dts:1.2.3")
+  jvm2dts("com.codeborne:jvm2dts:1.2.5")
+}
+
+sourceSets {
+  getByName("main").apply {
+    java.srcDirs("src")
+    resources.srcDirs("src", "i18n", "ui/static").exclude("**/*.kt")
+  }
+  getByName("test").apply {
+    java.srcDirs("test")
+    resources.srcDirs("test").exclude("**/*.kt")
+  }
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions {
+    jvmTarget = "11"
+    freeCompilerArgs = listOf("-Xjsr305=strict -progressive")
+    javaParameters = true
+  }
+  finalizedBy("generateTSTypes")
+}
+
+tasks.register("generateTSTypes") {
+  dependsOn("classes")
+  doLast {
+    val excludeClassNamesRegex = ".*(Service|Repository|Controller|Logger|Job|Module)\$"
+    val packagesToGenerateTypesFor = "auth"
+
+    File("ui/api/types.ts").writeText(ByteArrayOutputStream().use { out ->
+      project.exec {
+        standardOutput = out
+        commandLine = """java -classpath ${(jvm2dts + sourceSets.main.get().runtimeClasspath).asPath}
+          jvm2dts.Main -exclude $excludeClassNamesRegex 
+          $packagesToGenerateTypesFor""".split("\\s+".toRegex())
+      }
+      out.toString()
+    })
+  }
+}
+
+val test by tasks.getting(Test::class) {
+  useJUnitPlatform()
+  exclude("ui/**")
+}
+
+tasks.register<Test>("e2eTest") {
+  useJUnitPlatform()
+  include("ui/**")
+  if (project.hasProperty("headless"))
+    systemProperties["chromeoptions.args"] = "--headless,--no-sandbox,--disable-gpu"
+  systemProperties["webdriver.chrome.logfile"] = "build/reports/chromedriver.log"
+  systemProperties["webdriver.chrome.verboseLogging"] = "true"
 }
 
 tasks.register("downloadDeps") {
