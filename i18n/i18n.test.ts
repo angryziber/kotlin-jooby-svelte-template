5874799e9ac23136c8f6984129f16284091c2c52
Anton Keks <anton@codeborne.com>
2021-04-28 17:05:34 +0300

    use @web/test-runner with i18n tests
    TODO: need to find a way how to import ArrayExtensions globally, probaly using testRunnerHtml

--- i18n/i18n.test.ts
+++ i18n/i18n.test.ts
@@ -1,4 +1,6 @@
-import {translate, ensureSupportedLang} from './i18n'
+import {expect} from 'chai'
+import '../ui/shared/ArrayExtensions'
+import {ensureSupportedLang, translate} from './i18n'
 import langs from './langs'
 
 function createEntry(lang: string, key: string, text: string) {
@@ -12,56 +14,56 @@ function createEntry(lang: string, key: string, text: string) {
   keyWalker[lastKey] = text
 }
 
-test("test entry creation utility", () => {
+it("test entry creation utility", () => {
   createEntry('en', 'enkeytesting.testing.text', 'works!')
-  expect(langs['en']['enkeytesting']['testing']['text']).toEqual('works!')
+  expect(langs['en']['enkeytesting']['testing']['text']).to.equal('works!')
 })
 
-test('backend translate', () => {
-  expect(translate('en', 'login.submit')).toBe('Login')
-  expect(translate('en', 'login.otp.message', {values: {otp: '000111'}})).toBe('Your one-time password is 000111')
+it('backend translate', () => {
+  expect(translate('en', 'login.submit')).to.equal('Login')
+  expect(translate('en', 'login.otp.message', {values: {otp: '000111'}})).to.equal('Your one-time password is 000111')
 })
 
-test('backend translate with unsupported lang', () => {
-  expect(translate('??', 'login.submit')).toBe('Login')
+it('backend translate with unsupported lang', () => {
+  expect(translate('??', 'login.submit')).to.equal('Login')
 })
 
-test('translation should fall back to en', () => {
+it('translation should fall back to en', () => {
   createEntry('en', 'key.that.exists.only.in.en', 'Tere {name}!')
-  expect(translate('et', 'key.that.exists.only.in.en', {values: {name: 'Jaan'}})).toBe('Tere Jaan!')
+  expect(translate('et', 'key.that.exists.only.in.en', {values: {name: 'Jaan'}})).to.equal('Tere Jaan!')
 })
 
-test('if translation fails it should return translation key', () => {
+it('if translation fails it should return translation key', () => {
   const nonExistingKey = 'some.key.that.does.not.exist'
-  expect(translate('et', nonExistingKey)).toBe(nonExistingKey)
+  expect(translate('et', nonExistingKey)).to.equal(nonExistingKey)
 })
 
-test('ensureSupportedLang', () => {
-  expect(ensureSupportedLang('en')).toBe('en')
-  expect(ensureSupportedLang('??')).toBe('en')
+it('ensureSupportedLang', () => {
+  expect(ensureSupportedLang('en')).to.equal('en')
+  expect(ensureSupportedLang('??')).to.equal('en')
 })
 
-test('translate strings with plurals', () => {
+it('translate strings with plurals', () => {
   const key = 'bup.bap.plural.test'
   createEntry('en', key,
     'Testing {count|zero:nothing|one:a single translation|other:# translations!}')
   createEntry('en', key + '2',
     'Testing {count|zero:nothing|one:a single translation|other:# translations but don\'t \#change this!}')
-  expect(translate('en', key, {values: {count: 0}})).toBe("Testing nothing")
-  expect(translate('en', key, {values: {count: 1}})).toBe("Testing a single translation")
-  expect(translate('en', key, {values: {count: 5}})).toBe("Testing 5 translations!")
+  expect(translate('en', key, {values: {count: 0}})).to.equal("Testing nothing")
+  expect(translate('en', key, {values: {count: 1}})).to.equal("Testing a single translation")
+  expect(translate('en', key, {values: {count: 5}})).to.equal("Testing 5 translations!")
 
-  expect(translate('en', key + '2', {values: {count: 12}})).toBe("Testing 12 translations but don't #change this!")
+  expect(translate('en', key + '2', {values: {count: 12}})).to.equal("Testing 12 translations but don't #change this!")
 })
 
-test('translate template with plurals and regular substitution', () => {
+it('translate template with plurals and regular substitution', () => {
   const key = 'messages.since'
   createEntry('en', key, 'Tere {username}! You have {n|one:# message|other:# messages} since {date}')
-  expect(translate('en', key, {values: {n: 5, date: '2020/01/27', username: 'Piret'}})).toEqual('Tere Piret! You have 5 messages since 2020/01/27')
+  expect(translate('en', key, {values: {n: 5, date: '2020/01/27', username: 'Piret'}})).to.equal('Tere Piret! You have 5 messages since 2020/01/27')
 })
 
-test('empty token', () => {
+it('empty token', () => {
   const key = 'messages.since'
   createEntry('en', key, '{n|zero:|one:one|other:# messages}')
-  expect(translate('en', key, {values: {n: 0}})).toEqual('')
+  expect(translate('en', key, {values: {n: 0}})).to.equal('')
 })
