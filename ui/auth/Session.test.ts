8caa877c3e32237da6dfb43a70ad5553b54afc20
Anton Keks <anton@codeborne.com>
2021-04-29 12:16:40 +0300

    convert Session.test.ts

--- ui/auth/Session.test.ts
+++ ui/auth/Session.test.ts
@@ -1,24 +1,26 @@
 import session, {finishLogin, User} from './Session'
 import router from '../routing/Router'
+import {SinonStub, stub} from 'sinon'
+import {expect} from 'chai'
 
 const user: User = {
   id: 'id', role: 'user', login: 'login'
 }
 
 describe('finishLogin', () => {
-  beforeEach(() => {
-    jest.spyOn(router, 'navigateTo')
-  })
+  let navigateTo: SinonStub
+  beforeEach(() => navigateTo = stub(router, 'navigateTo'))
+  afterEach(() => navigateTo.restore())
 
   it('redirects to user.role', async () => {
     await finishLogin(user)
-    expect(session.user).toEqual(user)
-    expect(router.navigateTo).toBeCalledWith(user.role)
+    expect(session.user).to.eq(user)
+    expect(router.navigateTo).calledWith(user.role)
   })
 
   it('redirects to provided page', async () => {
     await finishLogin(user, 'some/page')
-    expect(session.user).toEqual(user)
-    expect(router.navigateTo).toBeCalledWith('some/page')
+    expect(session.user).to.eq(user)
+    expect(router.navigateTo).calledWith('some/page')
   })
 })
