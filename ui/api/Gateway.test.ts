362acc298a0f59431176783b545f8e4c7eab3752
Anton Keks <anton@codeborne.com>
2021-04-29 12:05:07 +0300

    make more gateway tests work

--- ui/api/Gateway.test.ts
+++ ui/api/Gateway.test.ts
@@ -2,10 +2,10 @@ import gateway, {headers} from './Gateway'
 import {expect} from 'chai'
 import {assert, fake, SinonStub, stub} from 'sinon'
 
-const mockResponse = {status: 200, headers: {get: () => undefined}, json: () => 'data'}
+const successfulResponse = {status: 200, headers: {get: () => undefined}, json: () => 'data'}
 
 it('extracts json', async () => {
-  const fetch = fake.resolves(mockResponse)
+  const fetch = fake.resolves(successfulResponse)
   const promise = gateway.request('/path', {body: {data: 'data'}}, fetch)
   expect(document.documentElement.classList.contains('loading')).to.equal(true)
   expect(await promise).to.equal('data')
@@ -15,20 +15,20 @@ it('extracts json', async () => {
 
 it('errors on api version mismatch', () => {
   window['apiVersion'] = '2.3'
-  const fetch = fake.resolves({...mockResponse, headers: {get: () => '2.2'}})
+  const fetch = fake.resolves({...successfulResponse, headers: {get: () => '2.2'}})
   const promise = gateway.request('/path', {body: {data: 'data'}}, fetch)
   window['apiVersion'] = undefined
   return promise.then(() => {throw 'should be rejected'}, e => expect(e).to.deep.equal({message: 'errors.apiVersionMismatch'}))
 })
 
 it('supports null json response', async () => {
-  const fetch = fake.resolves({...mockResponse, json: () => null})
+  const fetch = fake.resolves({...successfulResponse, json: () => null})
   const promise = gateway.request('/path', {body: {data: 'data'}}, fetch)
   expect(await promise).to.be.null
 })
 
 it('supports No Content response', async () => {
-  const fetch = fake.resolves({...mockResponse, status: 204})
+  const fetch = fake.resolves({...successfulResponse, status: 204})
   const promise = gateway.request('/path', {body: {data: 'data'}}, fetch)
   expect(await promise).to.be.undefined
 })
@@ -81,14 +81,12 @@ it('gives a network error', () => {
   })
 })
 
-/*
 describe('disabling form buttons on submit', () => {
   let form, button
   let fetch
 
   beforeEach(() => {
-    fetch = jest.fn().mockResolvedValue(mockResponse)
-
+    fetch = fake.resolves(successfulResponse)
     form = document.createElement('form')
     button = document.createElement('button')
     form.appendChild(button)
@@ -97,16 +95,16 @@ describe('disabling form buttons on submit', () => {
 
   it('disable any form button', async () => {
     const promise = gateway.request('/path', {method: 'POST'}, fetch)
-    expect(button).toBeDisabled()
+    expect(button.disabled).to.be.true
     await promise
-    expect(button).not.toBeDisabled()
+    expect(button.disabled).to.be.false
   })
 
   it('does not disable anything for GET', async () => {
     const promise = gateway.request('/path', {method: 'GET'}, fetch)
-    expect(button).not.toBeDisabled()
+    expect(button.disabled).to.be.false
     await promise
-    expect(button).not.toBeDisabled()
+    expect(button.disabled).to.be.false
   })
 
   it('does not enable disabled buttons', async () => {
@@ -115,17 +113,16 @@ describe('disabling form buttons on submit', () => {
     form.appendChild(disabledButton)
 
     const promise = gateway.request('/path', {method: 'PATCH'}, fetch)
-    expect(disabledButton).toBeDisabled()
+    expect(disabledButton.disabled).to.be.true
     await promise
-    expect(disabledButton).toBeDisabled()
+    expect(disabledButton.disabled).to.be.true
   })
 
   it('enable button even if it is removed from DOM', async () => {
     const promise = gateway.request('/path', {method: 'DELETE'}, fetch)
-    expect(button).toBeDisabled()
+    expect(button.disabled).to.be.true
     form.removeChild(button)
     await promise
-    expect(button).not.toBeDisabled()
+    expect(button.disabled).to.be.false
   })
 })
-*/
