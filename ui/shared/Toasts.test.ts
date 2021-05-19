ef54d20fd23fb9f8be2e4b3eb9b84280ac697d56
Anton Keks <anton@codeborne.com>
2021-05-19 17:35:01 +0300

    update test after bootstrap-5 related changes

--- ui/shared/Toasts.test.ts
+++ ui/shared/Toasts.test.ts
@@ -6,12 +6,12 @@ import {expect} from 'chai'
 it('renders regular toast', async () => {
   const {container} = render(Toasts)
   await act(() => showToast('Great success', {type: 'success'}))
-  expect(container.querySelector('.toasts-container .toast-success')!.innerHTML).to.contain('Great success')
+  expect(container.querySelector('.toast-container .bg-success')!.innerHTML).to.contain('Great success')
 })
 
 it('renders modal toast', async () => {
   const {container} = render(Toasts)
   await act(() => showToast('Great warning', {type: 'warning', modal: true}))
   expect(container.querySelector('.modal .alert-warning')!.innerHTML).to.contain('Great warning')
-  expect(container.querySelector('.toasts-container .modal')).to.be.null
+  expect(container.querySelector('.toast-container .modal')).to.be.null
 })
