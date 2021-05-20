b5215c3e29931dbbef0f9810a795e4f76e2952ef
Anton Keks, Margo Mitt <anton@codeborne.com, margo@codeborne.com>
2021-05-20 15:35:59 +0300

    use plugins from the above

--- snowpack.config.js
+++ snowpack.config.js
@@ -53,30 +53,7 @@ module.exports = {
     '**/ui/setup-tests.ts',
     '**/_*.scss'
   ],
-  plugins: [
-    ['@snowpack/plugin-run-script', {
-      cmd: 'sass ui/assets/scss:public/css --no-source-map --style=compressed', watch: 'sass ui/assets/scss:public/css --embed-source-map --watch'
-    }],
-    '@snowpack/plugin-typescript',
-    '@snowpack/plugin-svelte',
-    [
-      '@snowpack/plugin-run-script',
-      {cmd: 'svelte-check --output human --compiler-warnings ' + svelteIgnore.map(i => i + ':ignore').join(','), watch: '$1 --watch', output: 'stream'}
-    ],
-    [
-      'snowpack-plugin-rollup-bundle',
-      {
-        emitHtmlFiles: false,
-        preserveSourceFiles: false,
-        entrypoints: 'build/public/_dist_/ui/main.js',
-        extendConfig: config => {
-          config.outputOptions.entryFileNames = '[name].js'
-          config.outputOptions.assetFileNames = 'css/[name].[ext]'
-          return config
-        }
-      }
-    ]
-  ],
+  plugins,
   packageOptions: {
     knownEntrypoints: ['tslib'].concat(isTest ? ['sinon', 'chai', 'sinon-chai', '@testing-library/svelte'] : [])
   },
