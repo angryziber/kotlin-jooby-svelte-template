1511ff2f64f9aab699f8623dd56dfef8a38d76ca
Anton Keks <anton@codeborne.com>
2021-06-25 15:53:09 +0300

    we don't need tslib to target newer browsers

--- svelte.config.mjs
+++ svelte.config.mjs
@@ -1,6 +1,6 @@
-import sveltePreprocess from 'svelte-preprocess'
+import preprocess from 'svelte-preprocess'
 
 export default {
   dev: process.env.NODE_ENV === 'development',
-  preprocess: sveltePreprocess()
+  preprocess: preprocess({typescript: {compilerOptions: {target: 'esnext'}}})
 }
