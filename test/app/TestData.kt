9cd9c65422c1fa612a38cbd054f6011e5a4c4913
Anton Keks <anton@codeborne.com>
2021-06-29 15:17:27 +0300

    add javadoc to TestData

--- test/app/TestData.kt
+++ test/app/TestData.kt
@@ -1,9 +1,9 @@
 package app
 
-import auth.Role
 import auth.Role.USER
 import auth.User
 
+/** Reusable entities to use in tests to avoid specifying all params */
 object TestData {
   val user = User(login = "login", role = USER, lang = "en")
 }
