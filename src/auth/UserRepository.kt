4ecaf7793ad472a911385e172ee573845894594b
Anton Keks <anton@codeborne.com>
2021-06-29 15:38:23 +0300

    fix deprecated method usage

--- src/auth/UserRepository.kt
+++ src/auth/UserRepository.kt
@@ -12,24 +12,23 @@ class UserRepository(
   private val random: SecureRandom
 ): BaseRepository(db, "users") {
   private val extractor: ResultSet.() -> User = {
-    val id = getId()
-    User(id, getString("login"), getEnum("role"), getString("lang"), getString("name"), getString("email"), getInstant("createdAt"))
+    User(getId(), getString("login"), getEnum("role"), getString("lang"), getString("name"), getString("email"), getInstant("createdAt"))
   }
 
   fun get(id: UUID) = db.query(table, id, extractor)
 
   fun byCredentials(login: String, password: String): User? =
-    db.query(table, mapOf("login" to login.toLowerCase())) {
+    db.query(table, mapOf("login" to login.lowercase())) {
       extractor().takeIf { hashingService.hashPassword(password, it.id) == getString("passwordHash") }
     }.firstOrNull()
 
   fun byLogin(login: String): User? =
-    db.query(table, mapOf("login" to login.toLowerCase()), extractor).firstOrNull()
+    db.query(table, mapOf("login" to login.lowercase()), extractor).firstOrNull()
 
   fun generatePassword() = (1..16).joinToString("") { (33 + random.nextInt(127 - 33)).toChar().toString() }
 
   fun create(login: String, role: Role, lang: String, password: String, name: String? = null, email: String? = null): User =
-    User(role = role, lang = lang, login = login.toLowerCase(), name = name, email = email?.toLowerCase()).also { user ->
+    User(role = role, lang = lang, login = login.lowercase(), name = name, email = email?.lowercase()).also { user ->
       db.insert(table, mapOf(
         "id" to user.id,
         "login" to user.login,
@@ -40,10 +39,6 @@ class UserRepository(
         "passwordHash" to hashingService.hashPassword(password, user.id)
       ))
     }
-
-  fun byTagAndRoles(tagId: UUID, vararg roles: Role): List<User> =
-    db.select("select u.* from $table u join user_tags t on u.id = t.userId", mapOf("role" to roles, "tagId" to tagId),
-              "order by createdAt", extractor)
 
   fun updatePassword(id: UUID, password: String) =
     db.update(table, mapOf("id" to id), mapOf("passwordHash" to hashingService.hashPassword(password, id)))
