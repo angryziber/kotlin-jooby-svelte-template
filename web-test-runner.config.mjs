24da9bb08197ea41af6049bd5d6b75cb6c726bb7
Anton Keks <anton@codeborne.com>
2021-06-29 13:05:30 +0300

    run ui tests using puppeteer inside of Docker

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,12 +1,17 @@
 import snowpackPlugin from '@snowpack/web-test-runner-plugin'
+import {puppeteerLauncher} from '@web/test-runner-puppeteer'
 
-// NODE_ENV=test - Needed by "@snowpack/web-test-runner-plugin"
+// Needed by "@snowpack/web-test-runner-plugin"
 process.env.NODE_ENV = 'test'
 
 export default {
+  testsFinishTimeout: 10000,
+  // manual: true,
+  // open: true,
   plugins: [
     snowpackPlugin()
   ],
+  browsers: [puppeteerLauncher({launchOptions: {args: ['--no-sandbox']}})],
   testRunnerHtml: testFramework =>
     `<html>
       <body>
