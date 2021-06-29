1cfc63fe86810f861e5c83fd671ea456c2949b31
Anton Keks <anton@codeborne.com>
2021-06-29 15:31:52 +0300

    rename class to avoid confusion with TestData object

--- test/db/BaseModelTest.kt
+++ test/db/BaseModelTest.kt
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Test
 class BaseModelTest {
   @Test
   fun toValues() {
-    val data = TestData("Hello", 123)
+    val data = SomeData("Hello", 123)
     assertThat(data.toValues()).isEqualTo(mapOf("hello" to "Hello", "world" to 123))
   }
 
   @Test
   fun toValuesSkipping() {
-    val data = TestData("Hello", 123)
-    assertThat(data.toValuesSkipping(TestData::hello)).isEqualTo(mapOf("world" to 123))
-    assertThat(data.toValuesSkipping(TestData::hello, TestData::world)).isEqualTo(emptyMap<String, Any>())
+    val data = SomeData("Hello", 123)
+    assertThat(data.toValuesSkipping(SomeData::hello)).isEqualTo(mapOf("world" to 123))
+    assertThat(data.toValuesSkipping(SomeData::hello, SomeData::world)).isEqualTo(emptyMap<String, Any>())
   }
 
-  data class TestData(val hello: String, val world: Int)
+  data class SomeData(val hello: String, val world: Int)
 }
