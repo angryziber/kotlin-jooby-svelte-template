8a9c331c7046a2b4041d7ea802743dd5540663b8
Anton Keks <anton@codeborne.com>
2021-06-29 16:18:24 +0300

    add shared HttpClient with 5 sec connect timeout

--- src/app/Dependencies.kt
+++ src/app/Dependencies.kt
@@ -10,6 +10,9 @@ import io.jooby.Environment
 import io.jooby.Kooby
 import io.jooby.json.JacksonModule
 import io.jooby.put
+import java.net.http.HttpClient
+import java.time.Duration
+import java.time.temporal.ChronoUnit.SECONDS
 
 val objectMapper = JacksonModule.create().disable(WRITE_DATES_AS_TIMESTAMPS).setSerializationInclusion(NON_NULL)
   .configure(FAIL_ON_UNKNOWN_PROPERTIES, false).registerModule(KotlinModule())!!
@@ -17,6 +20,7 @@ val objectMapper = JacksonModule.create().disable(WRITE_DATES_AS_TIMESTAMPS).set
 fun Kooby.registerDependencies() {
   services.put(Environment::class.java, environment)
   services.put(Config::class.java, config)
+  services.put(HttpClient::class, HttpClient.newBuilder().connectTimeout(Duration.of(5, SECONDS)).build())
   services.put(ObjectMapper::class, objectMapper)
   install(JacksonModule(objectMapper))
 }
