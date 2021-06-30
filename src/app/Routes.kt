6f846147303b6cb033b873f2b1fa1d3a8e83a8de
Anton Keks <anton@codeborne.com>
2021-06-30 19:58:46 +0300

    move route helpers to Routes

--- src/app/Routes.kt
+++ src/app/Routes.kt
@@ -3,7 +3,10 @@ package app
 import auth.AuthController
 import auth.FakeLoginForTestingController
 import db.TransactionCoroutineContext
+import io.jooby.Context
+import io.jooby.Jooby
 import io.jooby.Kooby
+import io.jooby.require
 import kotlinx.coroutines.slf4j.MDCContext
 
 fun Kooby.registerRoutes() {
@@ -14,4 +17,13 @@ fun Kooby.registerRoutes() {
     mvc<AuthController>()
     if (environment.isTest) mvc<FakeLoginForTestingController>()
   }
+}
+
+interface Before {
+  fun before(ctx: Context)
+}
+
+private inline fun <reified T: Any> Jooby.mvc() = require<T>().also {
+  if (it is Before) before(it::before)
+  mvc(it)
 }
