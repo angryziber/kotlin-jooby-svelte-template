096e936e3d500d3e1a3062083898d0b1bbc73d04
Anton Keks <anton@codeborne.com>
2021-07-01 21:00:18 +0300

    introduce JsonHttpClient based on Java HttpClient

--- src/util/Json.kt
+++ src/util/Json.kt
@@ -3,10 +3,12 @@ package util
 import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.InputStream
 import java.io.OutputStream
+import kotlin.reflect.KClass
 
-inline fun <reified T: Any> ObjectMapper.parse(json: String) = readValue(json, T::class.java)
-inline fun <reified T: Any> ObjectMapper.parse(json: InputStream) = readValue(json, T::class.java)
+inline fun <reified T: Any> ObjectMapper.parse(json: String): T = readValue(json, T::class.java)
+inline fun <reified T: Any> ObjectMapper.parse(json: InputStream): T = readValue(json, T::class.java)
+inline fun <T: Any> ObjectMapper.parse(json: InputStream, type: KClass<T>): T = readValue(json, type.java)
 inline fun ObjectMapper.parse(json: InputStream) = readTree(json)
 
-inline fun ObjectMapper.stringify(o: Any) = writeValueAsString(o)
+inline fun ObjectMapper.stringify(o: Any?) = writeValueAsString(o)
 inline fun ObjectMapper.streamify(o: Any, out: OutputStream) = writeValue(out, o)
---
+++ src/util/JsonHttpClient.kt
@@ -0,0 +1,56 @@
+package util
+
+import app.objectMapper
+import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.future.await
+import org.slf4j.LoggerFactory
+import java.io.IOException
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpRequest.BodyPublishers.ofString
+import java.net.http.HttpResponse
+import java.net.http.HttpResponse.BodyHandlers.ofInputStream
+import java.time.Duration
+import java.time.Duration.ofSeconds
+import kotlin.reflect.KClass
+
+typealias RequestModifier =  HttpRequest.Builder.() -> HttpRequest.Builder
+
+class JsonHttpClient(
+  private val http: HttpClient,
+  private val urlPrefix: String = "",
+  val json: ObjectMapper = objectMapper,
+  private val reqModifier: RequestModifier = { this },
+  private val errorHandler: (HttpResponse<*>, String) -> Nothing = { res, body -> throw IOException("Failed with ${res.statusCode()}: $body") }
+) {
+  private val logger = LoggerFactory.getLogger(javaClass)
+
+  private fun jsonReq(urlSuffix: String) = HttpRequest.newBuilder().uri(URI.create("$urlPrefix$urlSuffix"))
+    .setHeader("Content-Type", "application/json; charset=UTF-8").setHeader("Accept", "application/json")
+    .timeout(ofSeconds(10)).reqModifier()
+
+  suspend fun <T: Any> request(urlSuffix: String, builder: HttpRequest.Builder.() -> HttpRequest.Builder, type: KClass<T>): T {
+    val req = jsonReq(urlSuffix).builder().build()
+    val start = System.nanoTime()
+    val res = http.sendAsync(req, ofInputStream()).await()
+    val ms = (System.nanoTime() - start) / 1000_000
+    if (res.statusCode() < 300) {
+      logger.info("${req.method()} $urlSuffix in $ms ms")
+      return json.parse(res.body(), type)
+    }
+    else {
+      val body = res.body().readBytes().decodeToString()
+      logger.error("Failed ${req.method()} $urlSuffix in $ms ms: ${res.statusCode()}: $body")
+      errorHandler(res, body)
+    }
+  }
+
+  suspend inline fun <reified T: Any> request(urlSuffix: String, noinline builder: HttpRequest.Builder.() -> HttpRequest.Builder) =
+    request(urlSuffix, builder, T::class)
+
+  suspend inline fun <reified T: Any> get(urlSuffix: String) = request<T>(urlSuffix) { GET() }
+  suspend inline fun <reified T: Any> post(urlSuffix: String, o: Any?) = request<T>(urlSuffix) { POST(ofString(json.stringify(o))) }
+  suspend inline fun <reified T: Any> put(urlSuffix: String, o: Any?) = request<T>(urlSuffix) { PUT(ofString(json.stringify(o))) }
+  suspend inline fun <reified T: Any> delete(urlSuffix: String, o: Any?) = request<T>(urlSuffix) { DELETE() }
+}
