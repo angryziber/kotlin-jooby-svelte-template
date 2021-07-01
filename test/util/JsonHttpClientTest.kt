3956dcacff66467cc158c40d1e56648178f3af92
Anton Keks <anton@codeborne.com>
2021-07-01 21:49:02 +0300

    add JsonHttpClientTest for a usage example

---
+++ test/util/JsonHttpClientTest.kt
@@ -0,0 +1,48 @@
+package util
+
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.ByteArrayInputStream
+import java.io.IOException
+import java.io.InputStream
+import java.net.http.HttpClient
+import java.net.http.HttpResponse
+import java.util.concurrent.CompletableFuture.completedFuture
+
+class JsonHttpClientTest {
+  val httpClient = mockk<HttpClient>()
+  val http = JsonHttpClient("http://some.host/v1", { setHeader("X-Custom-API", "123") }, http = httpClient)
+
+  @Test
+  fun get() {
+    val response = mockResponse(200, """{"hello": "World"}""")
+    every { httpClient.sendAsync<InputStream>(any(), any()) } returns completedFuture(response)
+
+    runBlocking {
+      val data = http.get<SomeData>("/some/data")
+      assertThat(data).isEqualTo(SomeData("World"))
+    }
+
+    coVerify { httpClient.sendAsync<String>(match { it.uri().toString() == "http://some.host/v1/some/data" }, any()) }
+  }
+
+  @Test
+  fun `http error`() {
+    val response = mockResponse(500, """{"error": "Error"}""")
+    every { httpClient.sendAsync<InputStream>(any(), any()) } returns completedFuture(response)
+
+    assertThrows<IOException> { runBlocking { http.get<SomeData>("/error") } }
+  }
+
+  data class SomeData(val hello: String)
+
+  private fun mockResponse(status: Int, body: String) = mockk<HttpResponse<InputStream>> {
+    every { statusCode() } returns status
+    every { body() } returns ByteArrayInputStream(body.toByteArray())
+  }
+}
