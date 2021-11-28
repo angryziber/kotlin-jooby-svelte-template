cd1565f78a31837c929ba3cd2328ada8f6f59d88
Anton Keks <anton@codeborne.com>
2021-11-28 12:14:58 +0200

    closing transactions in case of blocking requests before sending the response out
    Unfortunately, this cannot currently be done for non-blocking requests: https://github.com/jooby-project/jooby/issues/2465

--- src/db/RequestTransactionHandler.kt
+++ src/db/RequestTransactionHandler.kt
@@ -10,12 +10,19 @@ class RequestTransactionHandler: Extension {
     before {
       val tx = Transaction(db).attachToRequest()
       ctx.onComplete {
+        // close tx for non-blocking (coroutine) requests
         tx.close(commit = (200..399).contains(it.responseCode.value()))
       }
     }
 
     after {
-      Transaction.current()?.detachFromRequest() ?: Unit
+      if (ctx.isInIoThread) {
+        // Jooby shortcoming: "after" runs before coroutine handlers (https://github.com/jooby-project/jooby/issues/2465)
+        // so we just detach and will end the transaction in "onComplete" above
+        Transaction.current()!!.detachFromRequest()
+      }
+      else Transaction.current()!!.close(commit = failure == null)
+      Unit
     }
   }
 }
